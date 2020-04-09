3427d1ba504ec7a7b06de9b441752d972f01a4a0
洪学臣 <stevehum14@gmail.com>
2020-04-09 22:00:53 +0800

    add header and footer

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -2,13 +2,19 @@
   <div class="navbar navbar-default topnav">
     <div class="container">
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
         <a href="/" class="navbar-brand">
           <span class="title">{{logo.title}}</span>
           <img :src="logo.src" :alt="logo.title">
         </a>
       </div>
 
-      <div id="top-navbar-collapse" class="collapse navbar-collapse">
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
         <ul class="nav navbar-nav">
           <li v-for="(item,index) in navList" :class="{active:index === activeNavIndex}">
             <a href="#" @click="changeNavIndex(index)">{{item}}</a>
@@ -28,7 +34,8 @@
           title:'VuejsCaff'
         },
         navList:['社区', '头条', '问答', '教程'],
-        activeNavIndex:0
+        activeNavIndex:0,
+        showCollapsedNav:false  //添加一个是否显示折叠导航的开关 showCollapsedNav
       }
     },
     beforeCreate(){
@@ -37,7 +44,12 @@
     methods:{
       changeNavIndex(index){
         this.activeNavIndex = index
+      },
+      toggleNav(){
+        console.log(this.showCollapsedNav)
+        this.showCollapsedNav = !this.showCollapsedNav
       }
+
     }
   }
 
