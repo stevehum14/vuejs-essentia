d82f7af6ba4235c19dea18cdb74024223a64fc63
洪学臣 <stevehum14@gmail.com>
2020-04-11 16:25:22 +0800

    fix

---
+++ src/directives/index.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import validator from './validator'
+Vue.directive('validator', validator)
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@
 import Vue from 'vue'
 //引入 App.vue 的默认值
 import App from './App'
+//引入 路由
+import router from './router'
+import './directives'
 
 // 设置 false 以阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false
@@ -13,6 +16,7 @@ Vue.config.productionTip = false
 // 创建一个新的 Vue 实例
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
