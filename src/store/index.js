ee804e075b537029c54db48d5893c468377233dc
洪学臣 <stevehum14@gmail.com>
2020-09-12 18:12:07 +0800

    修改用户信息

--- src/store/index.js
+++ src/store/index.js
@@ -16,22 +16,35 @@ const mutations = {
     ls.setItem('user', user)
   },
   //添加 UPDATE_AUTH 来更改当前用户的登录状态
-  UPDATE_AUTH(state,auth){
+  UPDATE_AUTH(state, auth) {
     state.auth = auth
-    ls.setItem('auth',auth)
+    ls.setItem('auth', auth)
   }
 }
 
 const actions = {
-  login({ commit }, user) {
+  login({commit}, user) {
     if (user) commit('UPDATE_USER', user)
     //更新当前用户的登录状态为已登录
-  commit('UPDATE_AUTH',true)
+    commit('UPDATE_AUTH', true)
     router.push('/')
   },
-  logout({commit}){
-    commit('UPDATE_AUTH',false)
-    router.push({name: 'Home',params: {logout: true}})
+  //更新退出登录状态
+  logout({commit}) {
+    commit('UPDATE_AUTH', false)
+    router.push({name: 'Home', params: {logout: true}})
+  },
+  //更新个人信息
+  updateUser({state,commit},user){
+    //获取仓库的个人信息
+    const stateUser = state.user
+    //简单的数据类型判断
+    if(stateUser && typeof stateUser == 'object'){
+      //合并新旧个人信息，等价于 user = Object.assign({},stateUser,user)
+      user = { ...stateUser, ...user}
+    }
+    //更新个人信息
+    commit('UPDATE_USER',user)
   }
 }
 
